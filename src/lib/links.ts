020e0ed78f5842c4cbda15fcb663279fcd57af13
Tal Ohana <tal.ohana.x@gmail.com>
2023-01-27 16:58:18 +0200

    update siteurl

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -2,7 +2,6 @@ export const links = {
   github: 'https://github.com/talohana',
   linkedin: 'https://www.linkedin.com/in/talohana/',
   twitter: 'https://twitter.com/talohanax',
-  company: 'https://about.google.com',
 };
 
-export const siteUrl = 'https://horrorscope-co.vercel.app';
+export const siteUrl = 'https://horrorscope.vercel.app';
